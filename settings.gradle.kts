6fbbda7ca02397fefe547bd4c0d90fd15f19372f
ksqeib <i@iiii.fun>
2026-08-26 02:49:34 +0800

    chore: update Gradle wrapper to version 9.7.1 and add toolchain plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,6 +31,9 @@ pluginManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 rootProject.name = "HyperzoneLogin"
 
